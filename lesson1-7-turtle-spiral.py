9293dcacb05a2f42720a1997b9467508aa5be244
admins@Dell <prostopasta@gmail.com>
2020-05-19 19:25:03 +0300

    Урок 1. Задание 7 "Черепаха" - правильное

--- lesson1-7-turtle-spiral.py
+++ lesson1-7-turtle-spiral.py
@@ -1,10 +1,13 @@
 import turtle, math
 
-a = 1.5
-b = -2.4
+turtle.shape('turtle')
+a = 2.5
+b = -1
 t = 0
-while t <= 11*math.pi:
+turtle.right(90)
+while t <= 21*math.pi:
     x = (a + b*t) * math.cos(t)
     y = (a + b*t) * math.sin(t)
+    turtle.left(360/2/math.pi/5)
     turtle.goto(x,y)
-    t += 0.1
+    t += 0.2
