7596dcd30ed336112f54acc63a73679e43c0b670
admins@Dell <prostopasta@gmail.com>
2020-05-19 23:15:58 +0300

    Урок 1. Задание 9 "Черепаха": правильные многоугольники

--- lesson1-9-turtle-n-angles.py
+++ lesson1-9-turtle-n-angles.py
@@ -1,10 +1,28 @@
+import math
 import turtle
 
 turtle.shape('turtle')
+side = 100
+num = 10
 
-width = 6
-side = 12
-for i in range(10):
-    for j in range(4):
-        turtle.forward(side * (i + 1) + width * j/2)
-        turtle.left(90)
+
+def ext_radius(n=3):
+    if n == 3:
+        return side / math.sin(math.pi / n) / 2
+    else:
+        return ext_radius(n-1) / math.cos(math.pi / n)
+
+
+turtle.penup()
+for i in range(3, num + 1):
+    n_angle = 90 - 180 / i
+    turtle.forward(ext_radius(i))
+    turtle.pendown()
+    for j in range(i):
+        if j == 0:
+            turtle.left(180 - n_angle)
+        else:
+            turtle.left(180 - 2 * n_angle)
+        turtle.forward(side)
+    turtle.penup()
+    turtle.home()
