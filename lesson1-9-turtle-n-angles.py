c8c4fd3b0c7f66a571c1c3c120c7df85f0f5f2ce
prostopasta <past80@gmail.com>
2020-05-20 14:44:41 +0300

    Задание 9 выполнено. Конфигурация проекта изменилась

--- lesson1-9-turtle-n-angles.py
+++ lesson1-9-turtle-n-angles.py
@@ -2,31 +2,55 @@ import math
 import turtle
 
 turtle.shape('turtle')
-side = 200
+side3 = 36
 num = 10
+circle_edges = 36
+adjust = side3 / 3
 
 
-def n_side(n):
+def side(n):
     if n == 3:
-        return side
+        return side3
     else:
-        return 2 * n_radius(n - 1) * math.sin(math.pi / n) / math.cos(math.pi / n)
+#        return 2 * (radius(n-1)) * math.sin(math.pi / n) / math.cos(math.pi / n)  # высчитывать сторону на основе радиуса вписанной окружности
+        return 2 * (radius(n)) * math.sin(math.pi / n)
 
 
-def n_radius(n):
-    return n_side(n) / math.sin(math.pi / n) / 2
+def radius(n):
+    if n == 3:
+        return side(n) / math.sin(math.pi / n) / 2
+    else:
+        return radius(3) + (n - 3) * adjust
+
+
+def circle(n, r, solid=True):
+    turtle.penup()
+    turtle.home()
+    turtle.forward(r)
+    turtle.setheading(90)
+    turtle.pendown()
+    for i in range(n):
+        if not solid:
+            turtle.dot(2, "blue")
+            turtle.penup()
+        turtle.forward(2 * r * math.sin(math.pi / n))
+        turtle.pendown()
+        turtle.left(360 / n)
 
 
 turtle.penup()
-for i in range(3, num + 1):
+for i in range(3, num + 3):
     n_angle = 90 - 180 / i
-    turtle.forward(n_radius(i))
+    n_radius = radius(i)
+    n_side = side(i)
+    turtle.forward(radius(3) + (i - 3) * adjust)
     turtle.pendown()
     for j in range(i):
         if j == 0:
             turtle.left(180 - n_angle)
         else:
             turtle.left(180 - 2 * n_angle)
-        turtle.forward(n_side(i))
+        turtle.forward(n_side)
+#    circle(circle_edges, n_radius, False)  # рисовать описанную вокруг текущего многоугольника окружность
     turtle.penup()
     turtle.home()
