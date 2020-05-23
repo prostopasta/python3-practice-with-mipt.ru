60701ce895eb238703df4b0602fb3c1473bd6eea
admins@Dell <prostopasta@gmail.com>
2020-05-23 07:03:49 +0300

    Урок 1. Задание 12 "Черепаха": Пружина - Выполнено

--- lesson1-12-turtle-spring.py
+++ lesson1-12-turtle-spring.py
@@ -3,36 +3,50 @@ import turtle
 
 turtle.shape('turtle')
 turtle.speed('fastest')
-big_radius = 50
+big_radius = 60
 small_radius = big_radius / 5
 num_of_springs = 5
-circle_edges = 180
-circle_angle = 90
+circle_edges = 10     # Должно быть четным!
+start_angle = 90
+angle = 360 / circle_edges
 
 
 def spring(edges, rad, clockwise=True, solid=True):
     turtle.pendown()
     nn = 1
+
     while nn <= edges / 2:
         if not solid:
             turtle.dot(2, "blue")
             turtle.penup()
 
-        turtle.forward(2 * rad * math.sin(math.pi / (edges - 1)))
-        turtle.pendown()
+        distance = 2 * rad * math.sin(math.pi / edges)
+#        print('start(', nn, ') : pos=', turtle.position(), ', dist=', distance, ', ang=', turtle.heading())
+
+        turtle.forward(distance)
 
         if clockwise:
-            turtle.right(360 / edges)
+            turtle.right(angle)
         else:
-            turtle.left(360 / edges)
+            turtle.left(angle)
 
+#        print('end(', nn, ') : pos=', turtle.position(), ', dist=', distance, ', ang=', turtle.heading())
+
+        turtle.pendown()
         nn += 1
 
 
-turtle.back((2 * big_radius - small_radius) * num_of_springs / 2 - small_radius)
-turtle.setheading(circle_angle)
+turtle.forward(((big_radius - small_radius) * num_of_springs + small_radius))
+turtle.back(2 * ((big_radius - small_radius) * num_of_springs + small_radius))
+turtle.setheading(90 - angle / 2)
+#print('start: pos=', turtle.position(), ", ang=", turtle.heading())
 
 for n in range(num_of_springs):
     spring(circle_edges, big_radius)
+#    print('big: end pos=', turtle.position(), ", ang=", turtle.heading())
+
     if n != num_of_springs - 1:
         spring(circle_edges, small_radius)
+#        print('small: end pos=', turtle.position(), ", ang=", turtle.heading())
+
+turtle.home()
