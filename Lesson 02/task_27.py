62782ff66fb5ec8378a62f074dc48c4b16ce4346
admins@Dell <prostopasta@gmail.com>
2020-05-25 15:30:26 +0300

    Урок 2. Задания 27-28 "Робот": Выполнены

--- Lesson 02/task_27.py
+++ Lesson 02/task_27.py
@@ -5,7 +5,18 @@ from pyrob.api import *
 
 @task
 def task_7_5():
-    pass
+    i = 0
+    exit = False
+    move_right()
+    while not exit:
+        for j in range(i):
+            if wall_is_on_the_right():
+                exit = True
+                break
+            move_right()
+        i += 1
+        if not exit:
+            fill_cell()
 
 
 if __name__ == '__main__':
--- Lesson 02/task_28.py
+++ Lesson 02/task_28.py
@@ -5,7 +5,15 @@ from pyrob.api import *
 
 @task
 def task_7_6():
-    pass
+    x = 0
+    while True:
+        if x < 5:
+            move_right()
+        else:
+            break
+
+        if cell_is_filled():
+            x += 1
 
 
 if __name__ == '__main__':
