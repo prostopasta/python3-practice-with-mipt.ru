5853741a19b3bbf282d8ca694abdeceeea5488dc
admins@Dell <prostopasta@gmail.com>
2020-05-26 07:05:48 +0300

    Урок 2. Задания 29-30 "Робот": Выполнены

--- Lesson 02/task_29.py
+++ Lesson 02/task_29.py
@@ -5,7 +5,17 @@ from pyrob.api import *
 
 @task
 def task_7_7():
-    pass
+    x = 0
+    while True:
+        if (x < 3) and not wall_is_on_the_right():
+            move_right()
+        else:
+            break
+
+        if cell_is_filled():
+            x += 1
+        else:
+            x = 0
 
 
 if __name__ == '__main__':
--- Lesson 02/task_30.py
+++ Lesson 02/task_30.py
@@ -5,7 +5,31 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_9_3():
-    pass
+    size = 1
+    while not wall_is_on_the_right():
+        move_right()
+        size += 1
+
+    for y in range(size):
+        if (y + 1) % 2 == 0:
+            goRight = True
+        else:
+            goRight = False
+
+        for x in range(size):
+            if (x == y) or (size - x == y + 1):
+                pass
+            else:
+                fill_cell()
+
+            if goRight:
+                if not wall_is_on_the_right():
+                    move_right()
+            else:
+                if not wall_is_on_the_left():
+                    move_left()
+        if not wall_is_beneath():
+            move_down()
 
 
 if __name__ == '__main__':
