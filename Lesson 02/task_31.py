da605465140a955e3a471a585ad0f32a76f1b146
admins@Dell <prostopasta@gmail.com>
2020-05-26 11:30:03 +0300

    Урок 2. Задания 31-32 "Робот": Выполнены

--- Lesson 02/task_31.py
+++ Lesson 02/task_31.py
@@ -5,7 +5,25 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_8_30():
-    pass
+
+    goright = True
+    while goright:
+        while wall_is_beneath():
+            if goright:
+                if not wall_is_on_the_right():
+                    move_right()
+                else:
+                    goright = False
+            elif not goright:
+                if wall_is_on_the_left():
+                    if wall_is_beneath():
+                        return
+                else:
+                    move_left()
+
+        if not wall_is_beneath():
+            move_down()
+            goright = True
 
 
 if __name__ == '__main__':
--- Lesson 02/task_32.py
+++ Lesson 02/task_32.py
@@ -5,7 +5,31 @@ from pyrob.api import *
 
 @task(delay=0.01)
 def task_8_18():
-    pass
+    x = 0
+    y = 0
+    before_colored = 0
+
+    while wall_is_beneath():
+        x += 1
+
+        while not wall_is_above():
+            move_up()
+            if cell_is_filled():
+                before_colored += 1
+            else:
+                fill_cell()
+            y += 1
+
+        if y > 0:
+            move_down(y)
+            y = 0
+
+        if wall_is_above() and wall_is_beneath():
+            fill_cell()
+
+        move_right()
+
+    mov('ax', before_colored)
 
 
 if __name__ == '__main__':
