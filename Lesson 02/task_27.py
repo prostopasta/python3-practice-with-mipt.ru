3c8c02b8aae8fd412ea1c8fb6aebfd0e14b4c73c
admins@Dell <prostopasta@gmail.com>
2020-05-26 11:29:28 +0300

    Урок 2. Задание 27 "Робот": Рефакторинг

--- Lesson 02/task_27.py
+++ Lesson 02/task_27.py
@@ -3,20 +3,21 @@
 from pyrob.api import *
 
 
-@task
+@task(delay=0.01)
 def task_7_5():
-    i = 0
-    exit = False
+
     move_right()
-    while not exit:
-        for j in range(i):
-            if wall_is_on_the_right():
-                exit = True
-                break
-            move_right()
-        i += 1
-        if not exit:
+    i = 0
+
+    while True:
+        if not wall_is_on_the_right():
             fill_cell()
+        for x in range(i):
+            if not wall_is_on_the_right():
+                move_right()
+            else:
+                return
+        i += 1
 
 
 if __name__ == '__main__':
